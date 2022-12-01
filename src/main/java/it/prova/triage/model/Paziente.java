9b72025fe4bc626fc2cb6e610d1acfcad7b6e847
fabiopulcinelli <fabiopulcinelli2k2@gmail.com>
2022-12-01 17:10:56 +0100

    aggiunta model

---
+++ src/main/java/it/prova/triage/model/Paziente.java
@@ -0,0 +1,46 @@
+package it.prova.triage.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "paziente")
+@Data
+public class Paziente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "codiceFiscale")
+	private String codiceFiscale;
+	@Column(name = "dataRegistrazione")
+	private Date dataRegistrazione;
+	
+	public Paziente() {
+	}
+
+	public Paziente(String nome, String cognome, String codiceFiscale, Date dataRegistrazione) {
+		super();
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public Paziente(Long id) {
+		super();
+		this.id = id;
+	}
+}
